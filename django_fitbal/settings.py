827a28a2dc007b713dab7beae29eaf2a0fbf5045
otman <otman@DESKTOP-4RA473J>
2024-04-24 12:28:48 +0200

    add a customer and homepage

--- django_fitbal/settings.py
+++ django_fitbal/settings.py
@@ -6,10 +6,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger'
+    }
+except Exception as e:
+    pass
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-yog^&xty&y1_07w!(!=#42a+r)bryt2ajn(%1e+flq30@4-8w1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
